22f2fc91b4c3a5b0d4326bfe04336d1194ae7d30
belgoros <s.cambour@gmail.com>
2024-02-26 17:35:05 +0100

    Generate Wallet in the Payment context

---
+++ priv/repo/migrations/20240226143829_create_wallets.exs
@@ -0,0 +1,15 @@
+defmodule PaymentServer.Repo.Migrations.CreateWallets do
+  use Ecto.Migration
+
+  def change do
+    create table(:wallets) do
+      add :currency, :string
+      add :units, :integer
+      add :user_id, references(:users, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create index(:wallets, [:user_id, :currency], unique: true)
+  end
+end
