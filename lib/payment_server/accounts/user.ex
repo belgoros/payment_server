71f5326e9279a2403413e20226f23e46add95427
belgoros <s.cambour@gmail.com>
2024-02-26 17:35:31 +0100

    Add has_many relation to User

--- lib/payment_server/accounts/user.ex
+++ lib/payment_server/accounts/user.ex
@@ -1,10 +1,14 @@
 defmodule PaymentServer.Accounts.User do
+  @moduledoc false
   use Ecto.Schema
   import Ecto.Changeset
+
+  alias PaymentServer.Payment.Wallet
 
   schema "users" do
     field :email, :string
     field :name, :string
+    has_many :wallets, Wallet
 
     timestamps()
   end
