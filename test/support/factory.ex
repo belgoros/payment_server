45bd1c498c62ab98ab5d0c73079060f7bdef62bd
belgoros <s.cambour@gmail.com>
2024-02-26 17:35:43 +0100

    Create wallet_factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,11 +2,20 @@ defmodule PaymentServer.Factory do
   use ExMachina.Ecto, repo: PaymentServer.Repo
 
   alias PaymentServer.Accounts.User
+  alias PaymentServer.Payment.Wallet
 
   def user_factory do
     %User{
       email: sequence(:email, &"user-#{&1}@example.com"),
       name: sequence(:name, &"user-#{&1}")
+    }
+  end
+
+  def wallet_factory do
+    %Wallet{
+      units: 100,
+      currency: :EUR,
+      user: build(:user)
     }
   end
 end
