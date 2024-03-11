b265e002eb90f91a9a5d8eb397dcbcb49dee3bdb
Serguei Cambour <s.cambour@gmail.com>
2024-03-11 17:15:11 +0100

    Call dataloader in resolve function

--- lib/payment_server_web/graphql/types/wallet_type.ex
+++ lib/payment_server_web/graphql/types/wallet_type.ex
@@ -1,11 +1,15 @@
 defmodule PaymentServerWeb.Graphql.Types.WalletType do
   @moduledoc false
   use Absinthe.Schema.Notation
+  import Absinthe.Resolution.Helpers, only: [dataloader: 1]
+
+  alias PaymentServer.Accounts
 
   @desc "A User wallet"
   object :wallet do
     field(:id, non_null(:id))
     field(:currency, non_null(:currency_type))
+    field :user, non_null(:user), resolve: dataloader(Accounts)
   end
 
   @desc "Accepted currency values"
