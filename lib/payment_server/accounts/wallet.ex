fe865143aae2e351f63cd9a781a92b6feab8c7ac
Serguei Cambour <s.cambour@gmail.com>
2024-03-14 17:16:03 +0100

    Add assoc_constraint(:user) to the Wallet changeset

--- lib/payment_server/accounts/wallet.ex
+++ lib/payment_server/accounts/wallet.ex
@@ -21,6 +21,7 @@ defmodule PaymentServer.Accounts.Wallet do
     wallet
     |> cast(attrs, @available_attributes)
     |> validate_required(@required_attributes)
+    |> assoc_constraint(:user)
     |> unique_constraint(:currency,
       message: "User already has the wallet with the same currency!",
       name: :wallets_user_id_currency_index
