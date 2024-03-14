7735e6a26e44a8537eb723753c6bd997d267de07
Serguei Cambour <s.cambour@gmail.com>
2024-03-14 13:47:04 +0100

    Schedule fetching exchange rates

--- lib/payment_server/exchange/rates_monitor.ex
+++ lib/payment_server/exchange/rates_monitor.ex
@@ -8,12 +8,19 @@ defmodule PaymentServer.Exchange.RatesMonitor do
   alias PaymentServer.Exchange.Parser
 
   @default_server :rates_monitor
-  # @refresh_interval :timer.seconds(5)
+  @refresh_interval :timer.seconds(5)
+
+  defmodule State do
+    @moduledoc """
+    Exchange server state
+    """
+    defstruct from_currency: :usd, to_currency: :eur, rate: 1
+  end
 
   # Client Interface
   def start_link(opts \\ []) do
     opts = Keyword.put_new(opts, :name, @default_server)
-    GenServer.start_link(__MODULE__, %{}, opts)
+    GenServer.start_link(__MODULE__, %State{}, opts)
   end
 
   def get_rates(pid \\ @default_server, from_currency, to_currency) do
@@ -25,33 +32,34 @@ defmodule PaymentServer.Exchange.RatesMonitor do
   def init(old_state) do
     exchange_rates = fetch_rates(old_state)
     initial_state = Map.merge(old_state, exchange_rates)
-    # schedule_refresh()
+    schedule_refresh()
     {:ok, initial_state}
   end
 
   @impl true
   def handle_info(:refresh, state) do
     new_state = fetch_rates(state)
-    # schedule_refresh()
+    schedule_refresh()
     {:noreply, new_state}
   end
 
   @impl true
   def handle_call({:get_rates, state}, _from, old_state) do
-    new_state = old_state # or do come processing here
+    # or do come processing here
+    new_state = old_state
     to_caller = fetch_rates(state)
     {:reply, to_caller, new_state}
   end
 
-  # defp schedule_refresh do
-  #  Process.send_after(self(), :refresh, @refresh_interval)
-  # end
+  defp schedule_refresh do
+    Process.send_after(self(), :refresh, @refresh_interval)
+  end
 
-  defp fetch_rates(%{from_currency: from_currency, to_currency: to_currency}) do
+  defp fetch_rates(%{from_currency: from_currency, to_currency: to_currency} = state) do
     response = AlphaVantageApi.get_rates(from_currency, to_currency)
     rate = Parser.parse(response)
 
-    %{from_currency: from_currency, to_currency: to_currency, rate: rate}
+    %{state | from_currency: from_currency, to_currency: to_currency, rate: rate}
   end
 
   defp fetch_rates(%{}) do
