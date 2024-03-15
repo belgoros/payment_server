2f14515ef008ea8a14396c16c6b8b47b5571f960
Serguei Cambour <s.cambour@gmail.com>
2024-03-15 14:50:41 +0100

    Implement Wallets.total_worth_of_wallets_for/2
    This returns the total worth of all the user's wallets in a single currency

--- lib/payment_server/accounts.ex
+++ lib/payment_server/accounts.ex
@@ -21,6 +21,36 @@ defmodule PaymentServer.Accounts do
     Repo.all(User)
   end
 
+  def total_worth_of_wallets_for(%User{} = user, to_currency) do
+    user_wallets = get_user_wallets(user.id)
+    non_convertible_wallets = Enum.filter(user_wallets, &(&1.currency == to_currency))
+    convertible_wallets = user_wallets -- non_convertible_wallets
+
+    converted_amount = get_converted_amount(convertible_wallets, to_currency)
+    non_converted_amount = Enum.map(non_convertible_wallets, & &1.units) |> Enum.sum()
+
+    converted_amount + non_converted_amount
+  end
+
+  defp get_converted_amount(wallets_to_convert, to_currency) do
+    wallets_to_convert
+    |> Enum.reduce(0, fn wallet, acc ->
+      wallet.units * exchange_rate(wallet.currency, to_currency) + acc
+    end)
+  end
+
+  defp exchange_rate(from_currency, to_currency) do
+    rates_monitor_rates =
+      PaymentServer.Exchange.RatesMonitor.get_rates(from_currency, to_currency)
+
+    rates_monitor_rates.rate
+  end
+
+  defp get_user_wallets(id) do
+    query = from(w in Wallet, where: w.user_id == ^id)
+    Repo.all(query)
+  end
+
   @doc """
   Gets a single user.
 
