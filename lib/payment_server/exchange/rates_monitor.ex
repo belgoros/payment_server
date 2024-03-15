44ee10a35361ab70f0217ef12c5d1d21ccb6a828
Serguei Cambour <s.cambour@gmail.com>
2024-03-15 14:38:22 +0100

    Use State module struct to keep state

--- lib/payment_server/exchange/rates_monitor.ex
+++ lib/payment_server/exchange/rates_monitor.ex
@@ -14,7 +14,7 @@ defmodule PaymentServer.Exchange.RatesMonitor do
     @moduledoc """
     Exchange server state
     """
-    defstruct from_currency: :usd, to_currency: :eur, rate: 1
+    defstruct from_currency: :usd, to_currency: :eur, rate: 1.0
   end
 
   # Client Interface
@@ -24,7 +24,10 @@ defmodule PaymentServer.Exchange.RatesMonitor do
   end
 
   def get_rates(pid \\ @default_server, from_currency, to_currency) do
-    GenServer.call(pid, {:get_rates, %{from_currency: from_currency, to_currency: to_currency}})
+    GenServer.call(
+      pid,
+      {:get_rates, %State{from_currency: from_currency, to_currency: to_currency}}
+    )
   end
 
   # Server Callbacks
@@ -45,7 +48,6 @@ defmodule PaymentServer.Exchange.RatesMonitor do
 
   @impl true
   def handle_call({:get_rates, state}, _from, old_state) do
-    # or do come processing here
     new_state = old_state
     to_caller = fetch_rates(state)
     {:reply, to_caller, new_state}
@@ -60,9 +62,5 @@ defmodule PaymentServer.Exchange.RatesMonitor do
     rate = Parser.parse(response)
 
     %{state | from_currency: from_currency, to_currency: to_currency, rate: rate}
-  end
-
-  defp fetch_rates(%{}) do
-    %{}
   end
 end
