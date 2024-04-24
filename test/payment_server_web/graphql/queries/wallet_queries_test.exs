be46583c89047f5d325b15ffb3f35aa1bd3210f5
belgoros <s.cambour@gmail.com>
2024-04-24 18:34:18 +0200

    Add a test for walletsTotal query

--- test/payment_server_web/graphql/queries/wallet_queries_test.exs
+++ test/payment_server_web/graphql/queries/wallet_queries_test.exs
@@ -27,8 +27,8 @@ defmodule PaymentServerWeb.Graphql.Queries.WalletQueriesTest do
   """
 
   @wallets_by_currency_query """
-  query {
-    wallets_by_currency(currency: EUR) {
+  query($currency: CurrencyType!) {
+    wallets_by_currency(currency: $currency) {
      id
      currency
      units
@@ -36,8 +36,17 @@ defmodule PaymentServerWeb.Graphql.Queries.WalletQueriesTest do
    }
   """
 
+  @wallets_total_query """
+  query($currency: CurrencyType!, $userId: ID!) {
+    walletsTotal(currency: $currency, userId: $userId) {
+      currency
+      total
+    }
+  }
+  """
+
   describe "Listing wallets" do
-    test "It should return a list of wallets", %{conn: _conn} do
+    test "It should return a list of wallets" do
       wallet = insert(:wallet)
 
       {:ok, response} =
@@ -59,7 +68,7 @@ defmodule PaymentServerWeb.Graphql.Queries.WalletQueriesTest do
       assert expected === response
     end
 
-    test "It should return a list of wallets with their users", %{conn: _conn} do
+    test "It should return a list of wallets with their users" do
       wallet = insert(:wallet)
 
       {:ok, response} =
@@ -85,16 +94,54 @@ defmodule PaymentServerWeb.Graphql.Queries.WalletQueriesTest do
       assert expected === response
     end
 
-    test "it should return a list of wallets by currency", %{conn: _conn} do
+    test "it should return a list of wallets by currency" do
       insert_list(3, :wallet, %{currency: :EUR})
       insert(:wallet, currency: :USD)
 
+      query_input = %{
+        "currency" => "EUR"
+      }
+
       {:ok, response} =
-        Absinthe.run(@wallets_by_currency_query, PaymentServerWeb.Schema)
+        Absinthe.run(@wallets_by_currency_query, PaymentServerWeb.Schema, variables: query_input)
 
       wallets = get_in(response, [:data, "wallets_by_currency"])
 
       assert 3 == length(wallets)
+    end
+  end
+
+  describe "wallets total" do
+    test "it should return a total of user wallets in the specified currency" do
+      user = insert(:user)
+
+      insert(:wallet,
+        currency: :EUR,
+        units: Decimal.from_float(10.0),
+        user: user
+      )
+
+      insert(:wallet,
+        currency: :USD,
+        units: Decimal.from_float(50.0),
+        user: user
+      )
+
+      query_input = %{
+        "currency" => "EUR",
+        "userId" => user.id
+      }
+
+      {:ok, response} =
+        Absinthe.run(@wallets_total_query, PaymentServerWeb.Schema, variables: query_input)
+
+      expected = %{
+        data: %{
+          "walletsTotal" => get_in(response, [:data, "walletsTotal"])
+        }
+      }
+
+      assert response == expected
     end
   end
 end
